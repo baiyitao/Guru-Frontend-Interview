621112f5c30745168c77dfad42caacc54701f99f
baiyitao <tud46571@temple.edu>
2017-11-14 02:13:04 -0500

    0.0.1

---
+++ main.js
@@ -0,0 +1,25 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope, $http) {
+
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+    $http.get('http://interviewtest.getguru.com/seismic/data.json').then(function(data) {
+        $scope.data = data.data;
+
+        for (var i = 0; i < $scope.data.length; i++) {
+            $scope.data[i].time = new Date($scope.data[i].time)
+            $scope.data[i].timeBefore = $scope.data[i].time.getFullYear() + "/" + $scope.data[i].time.getMonth() + "/" + ($scope.data[i].time.getDay() + 1);
+            $scope.data[i].time = monthNames[$scope.data[i].time.getMonth()] + " " + ($scope.data[i].time.getDay() + 1) + ", " + $scope.data[i].time.getFullYear() + " @ " + $scope.data[i].time.getHours() + ":" + $scope.data[i].time.getMinutes();
+
+            // console.log($scope.data[i].timeBefore);
+        }
+        // console.log($scope.data);
+    });
+
+    $scope.latlon = function(x, y) {
+        alert("lat: " + x + ", lon: " + y)
+    }
+
+});
